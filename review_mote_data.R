6f6ba0b4604d10e131af4a1f81bed3c0fff25741
Jim Lutz <jdlutz@hotwaterresearch.net>
2017-11-23 12:12:22 -0800

    found spreadsheets with house and location data. start review_mote_data.R to summarize mote data

---
+++ review_mote_data.R
@@ -0,0 +1,58 @@
+# review_mote_data.R
+# script to review information in the mote files in
+# /home/jiml/HotWaterResearch/projects/HWDS monitoring/retrieve_field_data/data/by_sensorID
+# Jim Lutz "Thu Nov 23 11:37:13 2017"
+
+# set packages & etc
+source("setup.R")
+
+# set up paths to working directories
+source("setup_wd.R")
+
+# path to mote files
+wd_mote_data = "/home/jiml/HotWaterResearch/projects/HWDS monitoring/retrieve_field_data/data/by_sensorID/"
+
+# load the DT_field_data_uuid data.table
+load(file = paste0(wd_data,"DT_field_data_uuid.RData"))
+
+# see what we got
+tables()
+  # NAME                NROW NCOL MB COLS                                  KEY
+  # [1,] DT_field_data_uuid 2,278    5  1 uuid,houseID,moteID,sensortype,source    
+  # Total: 1MB
+DT_field_data_uuid
+
+# count of motes by houseID
+DT_field_data_uuid[,length(unique(moteID)),by=houseID][order(houseID)]
+
+# list all the motes from houseID 7
+sort(unique(DT_field_data_uuid[houseID==7,moteID]))
+# [1] "x3234" "x3242" "x3259" "x325f" "x3262" "x32ad" "x32ae" "x3356" "x337a" "x34e9"
+# [11] "x356a" "x3595" "x35b4" "x35b5" "x35c6" "x388b"
+
+# motes for this house
+this_house = 7
+motes_this_house = sort(unique(DT_field_data_uuid[houseID==this_house,moteID]))
+
+# uuids for this mote
+this_mote = "x3242"
+DT_field_data_uuid[moteID==this_mote,uuid]
+
+# load data for this mote 
+mote_fn = paste0("RSmap.",this_mote,".raw.xz.RData")
+#  the DT_field_data_uuid data.table
+load(file = paste0(wd_mote_data, mote_fn))
+
+# see what it looks like
+head(data)
+str(data)
+# it's the data streams from 7 uuids
+# list the uuids
+data[3,3]
+data[[]]$uuid
+uuids=""
+for(d in 1:7) {uuids[d] = data[[d]]$uuid}
+
+# see if uuids matches from DT_field_data_uuid
+identical(uuids,DT_field_data_uuid[moteID==this_mote,uuid])
+  # [1] TRUE
